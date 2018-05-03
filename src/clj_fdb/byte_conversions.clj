d45e4c9783e56d275e0429a619136934f21416b0
Vedang Manerikar <ved.manerikar@gmail.com>
2018-05-03 09:11:07 +0530

    Core fns take arguments to un/marshall k/v
    This gives us the flexibility to pass in functions to convert the data
    to the correct format as per requirement.

--- src/clj_fdb/byte_conversions.clj
+++ src/clj_fdb/byte_conversions.clj
@@ -5,45 +5,61 @@
            com.apple.foundationdb.tuple.Tuple
            java.nio.ByteBuffer))
 
-(def ^:private ^:const byte-array (class (Utils/byteArray 0)))
+(def byte-array-class (class (Utils/byteArray 0)))
 
-(bs/def-conversion [com.apple.foundationdb.tuple.Tuple byte-array]
+(bs/def-conversion [com.apple.foundationdb.tuple.Tuple byte-array-class]
   [t]
   (ftup/pack ^Tuple t))
 
-(bs/def-conversion [byte-array com.apple.foundationdb.tuple.Tuple]
+(bs/def-conversion [byte-array-class com.apple.foundationdb.tuple.Tuple]
   [ba]
   (ftup/from-bytes ^"[B" ba))
 
-(bs/def-conversion [java.lang.Short byte-array]
+(bs/def-conversion [java.lang.Short byte-array-class]
   [short-int]
   (.. (ByteBuffer/allocate 2)
       (putShort short-int)
       array))
 
-(bs/def-conversion [byte-array java.lang.Short]
+(bs/def-conversion [byte-array-class java.lang.Short]
   [short-int-ba]
   (.. (ByteBuffer/wrap short-int-ba)
       (getShort)))
 
-(bs/def-conversion [java.lang.Integer byte-array]
+(bs/def-conversion [java.lang.Integer byte-array-class]
   [integer]
   (.. (ByteBuffer/allocate 4)
       (putInt integer)
       array))
 
-(bs/def-conversion [byte-array java.lang.Integer]
+(bs/def-conversion [byte-array-class java.lang.Integer]
   [integer-ba]
   (.. (ByteBuffer/wrap integer-ba)
       (getInt)))
 
-(bs/def-conversion [java.lang.Long byte-array]
+(bs/def-conversion [java.lang.Long byte-array-class]
   [long-int]
   (.. (ByteBuffer/allocate 8)
       (putLong long-int)
       array))
 
-(bs/def-conversion [byte-array java.lang.Long]
+(bs/def-conversion [byte-array-class java.lang.Long]
   [long-int-ba]
   (.. (ByteBuffer/wrap long-int-ba)
       (getLong)))
+
+(bs/def-conversion [nil java.lang.Long]
+  [_]
+  nil)
+
+(bs/def-conversion [nil java.lang.Integer]
+  [_]
+  nil)
+
+(bs/def-conversion [nil java.lang.Short]
+  [_]
+  nil)
+
+(bs/def-conversion [nil com.apple.foundationdb.tuple.Tuple]
+  [_]
+  nil)
--- src/clj_fdb/core.clj
+++ src/clj_fdb/core.clj
@@ -1,57 +1,95 @@
 (ns clj-fdb.core
   (:require [byte-streams :as bs]
+            [clj-fdb.byte-conversions :refer [byte-array-class]]
             [clj-fdb.transaction :as ftr])
   (:import [com.apple.foundationdb KeyValue Range Transaction TransactionContext]
-           com.apple.foundationdb.tuple.Tuple))
-
-;;; Load functions which teach byte-streams how to convert things to
-;;; byte-arrays.
-(load "byte_conversions")
+           java.lang.IllegalArgumentException))
 
 (defn set
   "Takes the following:
-  - TransactionContext `db`
+  - TransactionContext `tc`
   - key to be stored `k`
   - key to be stored `v`
 
   and stores `v` against `k` in FDB. Returns nil.
 
-  Converts k/v to byte-array using byte-streams library."
-  [^TransactionContext db k v]
+  Optionally, you can pass in `:keyfn` and `:valfn` to transform the
+  key/value to a byte-array. An exception is thrown if these fns don't
+  return a byte-array. If no conversion fn is provided, we attemp to
+  convert k/v to byte-array using `byte-streams/to-byte-array`."
+  [^TransactionContext tc k v &
+   {:keys [keyfn valfn]
+    :or {keyfn bs/to-byte-array
+         valfn bs/to-byte-array}}]
   (let [tr-fn (fn [^Transaction tr]
-                (ftr/set tr
-                         (bs/to-byte-array k)
-                         (bs/to-byte-array v)))]
-    (ftr/run db tr-fn)))
+                (let [k-ba (keyfn k)
+                      v-ba (valfn v)]
+                  (when-not (instance? byte-array-class k-ba)
+                    (throw (IllegalArgumentException.
+                            "The provided Key Fn did not return a byte-array on input")))
+                  (when-not (instance? byte-array-class v-ba)
+                    (throw (IllegalArgumentException.
+                            "The provided Val Fn did not return a byte-array on input")))
+                  (ftr/set tr k-ba v-ba)))]
+    (ftr/run tc tr-fn)))
 
 (defn get
   "Takes the following:
-  - TransactionContext `db`
+  - TransactionContext `tc`
   - key to be fetched `k`
 
-  and returns byte-array `v` against `k` in FDB. Caller is responsible
-  for de-structuring v correctly.
+  and returns byte-array `v` against `k` in FDB.
 
-  Converts k to byte-array using the byte-streams library."
-  [^TransactionContext db k]
+  Optionally, you can pass in `:keyfn` and `:valfn` as follows:
+
+  - `:keyfn` should take the key as input and transform it to a
+  byte-array. An exception is thrown if the return value is not a
+  byte-array. If no `:keyfn` is provided, we convert the key to a
+  byte-array using `byte-streams/to-byte-array`.
+
+  - `:valfn` should accept a byte-array and convert it as desired by
+  the caller. If no `:valfn` is provided, we return the byte-array as
+  is."
+  [^TransactionContext tc k &
+   {:keys [keyfn valfn]
+    :or {keyfn bs/to-byte-array
+         valfn identity}}]
   (let [tr-fn (fn [^Transaction tr]
-                (deref (ftr/get tr (bs/to-byte-array k))))]
-    (ftr/run db tr-fn)))
+                (let [k-ba (keyfn k)]
+                  (when-not (instance? byte-array-class k-ba)
+                    (throw (IllegalArgumentException.
+                            "The provided Key Fn did not return a byte-array on input")))
+                  (when-let [v (deref (ftr/get tr k-ba))]
+                    (valfn v))))]
+    (ftr/run tc tr-fn)))
 
 (defn clear
   "Takes the following:
-  - TransactionContext `db`
+  - TransactionContext `tc`
   - key to be cleared `k`
 
-  and clears the key from the db. Returns nil."
-  [^TransactionContext db k]
+  and clears the key from the db. Returns nil.
+
+  Optionally, you can pass in `:keyfn` as follows:
+
+  - `:keyfn` should take the key as input and transform it to a
+  byte-array. An exception is thrown if the return value is not a
+  byte-array. If no `:keyfn` is provided, we convert the key to a
+  byte-array using `byte-streams/to-byte-array`."
+  [^TransactionContext tc k &
+   {:keys [keyfn]
+    :or {keyfn bs/to-byte-array}}]
   (let [tr-fn (fn [^Transaction tr]
-                (ftr/clear-key tr (bs/to-byte-array k)))]
-    (ftr/run db tr-fn)))
+                (let [k-ba (keyfn k)]
+                  (when-not (instance? byte-array-class k-ba)
+                    (throw (IllegalArgumentException.
+                            "The provided Key Fn did not return a byte-array on input")))
+                  (ftr/clear-key tr k-ba)))]
+    (ftr/run tc tr-fn)))
 
 (defn get-range
   "Takes the following:
-  - TransactionContext `db`
+  - TransactionContext `tc`
   - Range of keys to fetch `rg`
 
   and returns a map of key/value pairs (byte-array->byte-array).
@@ -60,7 +98,7 @@
   key/value to the correct format. `:keyfn` should accept a byte-array
   representing the key, `:valfn` should accept a byte-array
   representing the value."
-  [^TransactionContext db ^Range rg &
+  [^TransactionContext tc ^Range rg &
    {:keys [keyfn valfn]
     :or {keyfn identity
          valfn identity}}]
@@ -70,4 +108,4 @@
                                  (keyfn (.getKey kv)) (valfn (.getValue kv))))
                         {}
                         (ftr/get-range tr rg)))]
-    (ftr/run db tr-fn)))
+    (ftr/run tc tr-fn)))
