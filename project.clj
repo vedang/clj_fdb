0596ea1aed4cf9792195ce1a9c6251b5941e6c6b
Vedang Manerikar <ved.manerikar@gmail.com>
2018-05-06 15:15:50 +0530

    Add configuration options for codox.
    - Publish the documentation at https://vedang.github.io/clj_fdb/

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject clj-fdb "0.1.0"
   :description "A thin Clojure wrapper for the Java API for FoundationDB."
+  :url "https://vedang.github.io/clj_fdb/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.apple.foundationdb/fdb-java "5.1.7"]
                  [byte-streams "0.2.3"]]
   :target-path "target/%s"
+  :codox {:source-uri "https://github.com/vedang/clj_fdb/blob/master/{filepath}#L{line}"
+          :namespaces [#"^clj-fdb\.(?!byte-conversions)" #"^examples"]}
   :profiles {:uberjar {:aot :all}})
