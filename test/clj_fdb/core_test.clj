9ef261113cdd5ae2b2cfa64cbd0a9eb9f3f54f28
xtreak <tir.karthi@gmail.com>
2018-05-08 16:26:55 +0530

    Add test for get-range and clear-range
    Closes #6

--- test/clj_fdb/core_test.clj
+++ test/clj_fdb/core_test.clj
@@ -3,8 +3,11 @@
             [clj-fdb.core :as fc]
             [clj-fdb.FDB :as cfdb]
             [clj-fdb.internal.util :as u]
+            [clj-fdb.range :as frange]
+            [clj-fdb.transaction :as ftr]
             [clj-fdb.tuple.tuple :as ftup]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all])
+  (:import com.apple.foundationdb.Transaction))
 
 (def ^:dynamic *prefix* nil)
 
@@ -54,3 +57,45 @@
                v))
         (fc/clear db k)
         (is (nil? (fc/get db k)))))))
+
+(deftest test-get-range
+  (testing "Test the best-case path for `fc/get-range`. End is exclusive."
+    (let [fdb (cfdb/select-api-version 510)
+          input-keys ["bar" "car" "foo" "gum"]
+          begin      (ftup/pack (ftup/from *prefix* "b"))
+          end        (ftup/pack (ftup/from *prefix* "g"))
+          rg         (frange/range begin end)
+          v          (int 1)
+          expected-map {"bar" v "car" v "foo" v}]
+      (with-open [db (cfdb/open fdb)]
+        (ftr/run db
+          (fn [^Transaction tr]
+            (doseq [k input-keys]
+              (let [k (ftup/from *prefix* k)]
+                (fc/set tr k v)))))
+
+        (is (= (fc/get-range db rg
+                             :keyfn (comp second ftup/get-items ftup/from-bytes)
+                             :valfn #(bs/convert %1 Integer))
+               expected-map))))))
+
+(deftest test-clear-range
+  (testing "Test the best-case path for `fc/clear-range`. End is exclusive."
+    (let [fdb (cfdb/select-api-version 510)
+          input-keys ["bar" "car" "foo" "gum"]
+          begin      (ftup/pack (ftup/from *prefix* "b"))
+          end        (ftup/pack (ftup/from *prefix* "g"))
+          rg         (frange/range begin end)
+          v          (int 1)
+          excluded-k "gum"]
+      (with-open [db (cfdb/open fdb)]
+        (ftr/run db
+          (fn [^Transaction tr]
+            (doseq [k input-keys]
+              (let [k (ftup/from *prefix* k)]
+                (fc/set tr k v)))))
+        (fc/clear-range db rg)
+
+        (is (= (fc/get db (ftup/from *prefix* excluded-k)
+                       :valfn #(bs/convert % Integer))
+               v))))))
