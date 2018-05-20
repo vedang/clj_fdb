5ae76b3a2b6a66bc104a9d24d8055cc3b5366151
Vedang Manerikar <ved.manerikar@gmail.com>
2018-05-20 11:58:27 +0530

    Add wrapper fns for com.apple.foundationdb.FDB

--- src/clj_fdb/FDB.clj
+++ src/clj_fdb/FDB.clj
@@ -1,18 +1,105 @@
 (ns clj-fdb.FDB
-  (:import com.apple.foundationdb.FDB))
+  (:import [com.apple.foundationdb FDB FDBDatabase NetworkOptions]
+           java.util.concurrent.Executor))
 
-(defn select-api-version
-  "Select the version for the client API."
+(defn ^NetworkOptions options
+  "Returns a set of options that can be set on a the FoundationDB API.
+  Generally, these options to the top level of the API affect the
+  networking engine and therefore must be set before the network
+  engine is started. The network is started by calls to startNetwork()
+  and implicitly by calls to open() and createCluster() (and their
+  respective variants)."
+  [db]
+  (.options db))
+
+(defn ^FDBDatabase select-api-version
+  "Select the version for the client API. An exception will be thrown
+  if the requested version is not supported by this implementation of
+  the API. As only one version can be selected for the lifetime of the
+  JVM, the result of a successful call to this method is always the
+  same instance of a FDB object.
+
+  Warning: When using the multi-version client API, setting an API
+  version that is not supported by a particular client library will
+  prevent that client from being used to connect to the cluster. In
+  particular, you should not advance the API version of your
+  application after upgrading your client until the cluster has also
+  been upgraded."
   [^Integer version]
   (FDB/selectAPIVersion version))
 
+(defn set-unclosed-warning
+  "Enables or disables the stderr warning that is printed whenever an
+  object with FoundationDB native resources is garbage collected
+  without being closed. By default, this feature is enabled."
+  [db warn-on-unclosed?]
+  (.setUnclosedWarning db warn-on-unclosed?))
+
+(defn create-cluster
+  "Connects to the cluster specified by the default `fdb.cluster` file.
+  If the FoundationDB network has not been started, it will be started
+  in the course of this call as if `startNetwork()` had been called.
+
+  Returns:
+
+  - a `CompletableFuture` that will be set to a FoundationDB Cluster.
+
+  Throws:
+
+  - `FDBException` - on errors encountered starting the FoundationDB
+  networking engine
+  - `java.lang.IllegalStateException` - if the network had been
+  previously stopped."
+  ([db]
+   (.createCluster db))
+  ([db cluster-file-path]
+   (.createCluster db cluster-file-path))
+  ([db cluster-file-path ^Executor e]
+   (.createCluster db cluster-file-path e)))
+
 (defn open
   "Initializes networking, connects with the default fdb.cluster file,
-  and opens the database."
-  [^FDB db]
-  (.open db))
+  and opens the database.
 
-(defn close
-  "Close the Database object and release any associated resources."
-  [^FDB db]
-  (.close db))
+  Returns:
+  - a `CompletableFuture` that will be set to a FoundationDB Database
+
+  Throws: `FDBException`"
+  ([db]
+   (.open db))
+  ([db cluster-file-path]
+   (.open db cluster-file-path))
+  ([db cluster-file-path ^Executor e]
+   (.open db cluster-file-path e)))
+
+(defn start-network
+  "Initializes networking. Can only be called once. This version of
+  startNetwork() will create a new thread and execute the networking
+  event loop on that thread. This method is called upon Database or
+  Cluster creation by default if the network has not yet been started.
+  If one wishes to control what thread the network runs on, one should
+  use the version of startNetwork() that takes an Executor.
+
+  Configuration of the networking engine can be achieved through calls
+  to the methods in NetworkOptions.
+
+  Throws:
+  - java.lang.IllegalStateException - if the network has already been
+  stopped
+  - FDBException
+
+  See Also: NetworkOptions."
+  ([db]
+   (.startNetwork db))
+  ([db ^Executor e]
+   (.startNetwork db e)))
+
+(defn stop-network
+  "Stops the FoundationDB networking engine. This can be called only
+  once -- the network cannot be restarted after this call. This call
+  blocks for the completion of the FoundationDB networking engine.
+
+  Throws:
+  - FDBException - on errors while stopping the network."
+  [db]
+  (.stopNetwork db))
