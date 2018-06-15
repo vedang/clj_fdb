2f3fd76dfe556cb174ef0f18c02757ce0b144f93
Vedang Manerikar <ved.manerikar@gmail.com>
2018-06-15 22:27:49 +0530

    Move appropriate libraries to dev profile

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://vedang.github.io/clj_fdb/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.apple.foundationdb/fdb-java "5.1.7"]
+  :dependencies [[com.apple.foundationdb/fdb-java "5.1.7"]
                  [byte-streams "0.2.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]
+                   :plugins [[lein-codox "0.10.4"]]}}
   :target-path "target/%s"
   :codox {:source-uri "https://github.com/vedang/clj_fdb/blob/master/{filepath}#L{line}"
           :namespaces [#"^examples" #"^clj-fdb\.(?!internal)"]
