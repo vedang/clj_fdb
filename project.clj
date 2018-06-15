12f69bfabb00a5eae5cac496438da2b5598d7780
Vedang Manerikar <ved.manerikar@gmail.com>
2018-06-15 23:51:34 +0530

    Add explicit dev dependency on ctl

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.apple.foundationdb/fdb-java "5.1.7"]
                  [byte-streams "0.2.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/tools.logging "0.4.1"]]
                    :plugins [[lein-codox "0.10.4"]]}}
   :target-path "target/%s"
   :codox {:source-uri "https://github.com/vedang/clj_fdb/blob/master/{filepath}#L{line}"
