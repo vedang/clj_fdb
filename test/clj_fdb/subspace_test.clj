b3348b0692bf451d0f656366a28212be42d7ded3
Pardeep Singh <pardeep@helpshift.com>
2018-06-16 21:05:24 +0530

    Add tests for Subspace wrappers

---
+++ test/clj_fdb/subspace_test.clj
@@ -0,0 +1,36 @@
+(ns clj-fdb.subspace-test
+  (:require [clojure.test :refer :all]
+            [byte-streams :as bs]
+            [clj-fdb.FDB :as cfdb]
+            [clj-fdb.core :as fc]
+            [clj-fdb.subspace.subspace :as fss]
+            [clj-fdb.tuple.tuple :as ftup]
+            [clj-fdb.internal.util :as u]))
+
+
+(deftest test-prefixed-subspace
+  (let [fdb (cfdb/select-api-version 510)
+        random-prefix (str "prefixed-subspace-test:"
+                           (u/rand-str 5))
+        prefix-subspace (fss/create-subspace (ftup/from random-prefix))]
+    (with-open [db (cfdb/open fdb)]
+      (fc/set db
+              (fss/pack prefix-subspace
+                        (ftup/from "test-key"))
+              "subspace value")
+      (is (= (fc/get db
+                     (fss/pack prefix-subspace
+                               (ftup/from "test-key"))
+                     :valfn #(bs/convert % String))
+             "subspace value"))
+      (is (fss/contains? prefix-subspace
+                         (fss/pack prefix-subspace
+                                   (ftup/from "test-key"))))
+      (is (= (fss/unpack prefix-subspace
+                         (fss/pack prefix-subspace
+                                   (ftup/from)))
+             (ftup/from)))
+      (is (= (fss/unpack prefix-subspace
+                         (fss/pack prefix-subspace
+                                   (ftup/from "test")))
+             (ftup/from "test"))))))
