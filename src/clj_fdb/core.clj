0cf5748835ab7f6e7680a69073cf10cc6345ea6a
Pardeep Singh <pardeep@helpshift.com>
2018-06-24 16:04:01 +0530

    Add Subspace utility functions
    - Add tests for utility functions.

--- src/clj_fdb/core.clj
+++ src/clj_fdb/core.clj
@@ -2,8 +2,11 @@
   (:refer-clojure :exclude [get set])
   (:require [byte-streams :as bs]
             [clj-fdb.internal.byte-conversions :refer [byte-array-class]]
-            [clj-fdb.transaction :as ftr])
+            [clj-fdb.transaction :as ftr]
+            [clj-fdb.subspace.subspace :as fsubspace])
   (:import [com.apple.foundationdb KeyValue Range Transaction TransactionContext]
+           [com.apple.foundationdb.subspace Subspace]
+           [com.apple.foundationdb.tuple Tuple]
            java.lang.IllegalArgumentException))
 
 (defn set
@@ -47,7 +50,6 @@
   byte-array. An exception is thrown if the return value is not a
   byte-array. If no `:keyfn` is provided, we convert the key to a
   byte-array using `byte-streams/to-byte-array`.
-
   - `:valfn` should accept a byte-array and convert it as desired by
   the caller. If no `:valfn` is provided, we return the byte-array as
   is."
@@ -120,3 +122,93 @@
   [^TransactionContext tc ^Range rg]
   (let [tr-fn (fn [^Transaction tr] (ftr/clear-range tr rg))]
     (ftr/run tc tr-fn)))
+
+(defn set-subspaced-key
+  "Takes the following:
+  - TransactionContext `tc`
+  - Subspace `s` which will be used to namespace the key
+  - Tuple `t` will be used along with `s` to construct key
+  - Value to be stored `v`
+
+  and stores `v` against key constructed using `s` and `t` in DB. Returns nil.
+
+  Optionally, you can pass in `:valfn` as follows:
+
+  - `:valfn` to transform the `v` to a byte-array. If `:valfn` is not provided,
+  `bs/to-byte-array` is used to transform `v`."
+  ([^TransactionContext tc ^Subspace s ^Tuple t v
+    & {:keys [valfn]
+       :or {valfn bs/to-byte-array}}]
+   (set tc t v
+        :keyfn #(fsubspace/pack s %)
+        :valfn valfn)))
+
+(defn get-subspaced-key
+  "Takes the following:
+  - TransactionContext `tc`
+  - Subspace `s` which will be used to namespace the key
+  - Tuple `t` will be used along with `s` to construct key
+
+  and returns byte-array `v` against key constructed using `s` and `t`.
+
+  Optionally, you can pass in `:valfn` as follows:
+
+  - `:valfn` should accept a byte-array and convert it as desired by the caller.
+  If no `:valfn` is provided, we return the byte-array as is."
+  ([^TransactionContext tc ^Subspace s ^Tuple t
+    & {:keys [valfn]
+       :or {valfn identity}}]
+   (get tc t
+        :keyfn #(fsubspace/pack s %)
+        :valfn valfn)))
+
+(defn clear-subspaced-key
+  "Takes the following:
+  - TransactionContext `tc`
+  - Subspace `s` which will used to namespace the key
+  - Tuple `t` will be used along with `s` to construct key
+
+  and clears the Subspaced key from db. Returns nil."
+  [^TransactionContext tc ^Subspace s ^Tuple t]
+  (clear tc t
+         :keyfn #(fsubspace/pack s %)))
+
+(defn get-subspaced-range
+  "Takes the following:
+  - TransactionContext `tc`
+  - Subspace `s` which will used to namespace the key
+  - Tuple `t` will be used along with `s` to construct key
+
+  and returns a map of key/value pairs (byte-array->byte-array).
+
+  Optionally, you can pass in `:keyfn` and `:valfn` as follows:
+
+  - `:keyfn` can be passed to transform key to correct format.
+  If no `:keyfn` is provided, we return the byte-array as is.
+  - `:valfn` can be passed to tranform value to correct format.
+  If no `:valfn` is provided, we return the byte-array as is."
+  [^TransactionContext tc ^Subspace s ^Tuple t
+   & {:keys [keyfn valfn]
+      :or {keyfn identity
+           valfn identity}}]
+  (let [subspaced-range (fsubspace/range s t)]
+    (get-range tc subspaced-range
+               :keyfn keyfn
+               :valfn valfn)))
+
+(defn clear-subspaced-range
+  "Takes the following:
+  - TransactionContext `tc`
+  - Subspace `s` which will used to namespace the key
+
+  and clears the range from db. Returns nil.
+
+  Optionally, you can pass in `:t` as follows:
+
+  - `:t` will be used along with `s` to construct namespaced key. `t` should be of type `Tuple`."
+  ([^TransactionContext tc ^Subspace s]
+   (let [subspaced-range (fsubspace/range s)]
+     (clear-range tc subspaced-range)))
+  ([^TransactionContext tc ^Subspace s ^Tuple t]
+   (let [subspaced-range (fsubspace/range s t)]
+     (clear-range tc subspaced-range))))
--- test/clj_fdb/core_test.clj
+++ test/clj_fdb/core_test.clj
@@ -6,7 +6,8 @@
             [clj-fdb.range :as frange]
             [clj-fdb.transaction :as ftr]
             [clj-fdb.tuple.tuple :as ftup]
-            [clojure.test :refer :all])
+            [clojure.test :refer :all]
+            [clj-fdb.subspace.subspace :as fsubspace])
   (:import com.apple.foundationdb.Transaction))
 
 (use-fixtures :each u/test-fixture)
@@ -82,3 +83,109 @@
         (is (= (fc/get db (ftup/from u/*test-prefix* excluded-k)
                        :valfn #(bs/convert % Integer))
                v))))))
+
+(deftest test-get-set-subspaced-key
+  (testing "Get/Set Subspaced Key using empty Tuple"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)]
+      (with-open [db (cfdb/open fdb)]
+        (fc/set-subspaced-key db
+                              prefixed-subspace
+                              (ftup/from)
+                              "value")
+        (is (= (fc/get-subspaced-key db
+                                     prefixed-subspace
+                                     (ftup/from)
+                                     :valfn #(bs/convert % String))
+               "value"))
+        (fc/set-subspaced-key db
+                              prefixed-subspace
+                              (ftup/from)
+                              "New value")
+        (is (= (fc/get-subspaced-key db
+                                     prefixed-subspace
+                                     (ftup/from)
+                                     :valfn #(bs/convert % String))
+               "New value")))))
+  (testing "Get/Set Subspaced Key using non-empty Tuple"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)]
+      (with-open [db (cfdb/open fdb)]
+        (fc/set-subspaced-key db
+                              prefixed-subspace
+                              (ftup/from "a")
+                              "value")
+        (= (fc/get-subspaced-key db
+                                 prefixed-subspace
+                                 (ftup/from "a")
+                                 :valfn #(bs/convert % String))
+           "value"))))
+  (testing "Get non-existent Subspaced Key"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)]
+      (with-open [db (cfdb/open fdb)]
+        (is (nil? (fc/get-subspaced-key db
+                                        prefixed-subspace
+                                        (ftup/from "a")
+                                        :valfn #(bs/convert % String))))))))
+
+(deftest test-clear-subspaced-key
+  (testing "Clear subspaced key"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)]
+      (with-open [db (cfdb/open fdb)]
+        (fc/set-subspaced-key db
+                              prefixed-subspace
+                              (ftup/from)
+                              "value")
+        (is (= (fc/get-subspaced-key db
+                                     prefixed-subspace
+                                     (ftup/from)
+                                     :valfn #(bs/convert % String))
+               "value"))
+        (fc/clear-subspaced-key db
+                                prefixed-subspace
+                                (ftup/from))
+        (is (nil? (fc/get-subspaced-key db
+                                        prefixed-subspace
+                                        (ftup/from)
+                                        :valfn #(bs/convert % String))))))))
+
+(deftest test-get-subspaced-range
+  (testing "Get subspaced range"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)
+          input-keys ["bar" "car" "foo" "gum"]
+          v "10"
+          expected-map {"bar" v "car" v "foo" v "gum" v}]
+      (with-open [db (cfdb/open fdb)]
+        (doseq [k input-keys]
+          (fc/set-subspaced-key db prefixed-subspace (ftup/from k) v))
+        (is (= (fc/get-subspaced-range db prefixed-subspace (ftup/from)
+                                       :keyfn (comp last ftup/get-items ftup/from-bytes)
+                                       :valfn #(bs/convert % String))
+               expected-map))))))
+
+(deftest test-clear-subspaced-range
+  (testing "Clear subspaced range"
+    (let [fdb (cfdb/select-api-version 510)
+          random-prefixed-tuple (ftup/from u/*test-prefix* "subspace" (u/rand-str 5))
+          prefixed-subspace (fsubspace/create-subspace random-prefixed-tuple)
+          input-keys ["bar" "car" "foo" "gum"]
+          v "10"
+          expected-map {"bar" v "car" v "foo" v "gum" v}]
+      (with-open [db (cfdb/open fdb)]
+        (doseq [k input-keys]
+          (fc/set-subspaced-key db prefixed-subspace (ftup/from k) v))
+        (is (= (fc/get-subspaced-range db prefixed-subspace (ftup/from)
+                                       :keyfn (comp last ftup/get-items ftup/from-bytes)
+                                       :valfn #(bs/convert % String))
+               expected-map))
+        (fc/clear-subspaced-range db prefixed-subspace)
+        (is (nil? (fc/get-subspaced-key db prefixed-subspace (ftup/from)
+                                        :valfn #(bs/convert % String))))))))
