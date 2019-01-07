0948bf5ee744d0540f385f70cb8aa28f7853816b
Vedang Manerikar <ved.manerikar@gmail.com>
2019-01-07 10:02:02 +0530

    Update dep versions
    - move byte-streams to 0.2.4
    - change clojure dep to 1.10.0
    - change codox to 0.10.5

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.foundationdb/fdb-java "6.0.15"]
-                 [byte-streams "0.2.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                 [byte-streams "0.2.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
                                   [org.clojure/tools.logging "0.4.1"]]
-                   :plugins [[lein-codox "0.10.4"]]}}
+                   :plugins [[lein-codox "0.10.5"]]}}
   :target-path "target/%s"
   :codox {:source-uri "https://github.com/vedang/clj_fdb/blob/master/{filepath}#L{line}"
           :namespaces [#"^examples" #"^clj-fdb\.(?!internal)"]
